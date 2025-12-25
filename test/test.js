3a552045d7c3a8be17ff16116b7623f7fcce6fc4
Paul <paul@arcticnotes.com>
2025-12-25 02:47:00 -0500

    add .npmignore

--- test/test.js
+++ test/test.js
@@ -5,15 +5,15 @@ import { WindowsScriptingHost} from '@arcticnotes/node-wsh';
 TEST( 'smoke-test', async() => {
 	const wsh = await WindowsScriptingHost.connect();
 	try {
-		const WScript = wsh.WScript;
-		const GetObject = wsh.GetObject;
-		const Enumerator = wsh.Enumerator;
-
+		const { WScript, GetObject, Enumerator} = wsh;
 		console.log( WScript.Version);
 		ASSERT.equal( typeof WScript.Version, 'string');
 		const procs = GetObject( "winmgmts:\\\\.\\root\\cimv2").ExecQuery( 'SELECT ProcessId, Name FROM Win32_Process');
-		for( const enumerator = new Enumerator( procs); !enumerator.atEnd(); enumerator.moveNext())
+		for( const enumerator = new Enumerator( procs); !enumerator.atEnd(); enumerator.moveNext()) {
 			console.log( `${ enumerator.item().ProcessId}: ${ enumerator.item().Name}`);
+			ASSERT.equal( typeof enumerator.item().ProcessId, 'number');
+			ASSERT.equal( typeof enumerator.item().Name, 'string');
+		}
 	} finally {
 		await wsh.disconnect();
 	}
