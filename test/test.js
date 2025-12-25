9358c41f07582785bdad5d3f73055776f042d0bd
Paul <paul@arcticnotes.com>
2025-12-25 00:50:36 -0500

    add apply()

--- test/test.js
+++ test/test.js
@@ -6,8 +6,10 @@ TEST( 'smoke-test', async() => {
 	const wsh = await WindowsScriptingHost.connect();
 	try {
 		const WScript = wsh.WScript;
+		const GetObject = wsh.GetObject;
 		console.log( WScript.Version);
 		ASSERT.equal( typeof WScript.Version, 'string');
+		console.log( GetObject( "winmgmts:\\\\.\\root\\cimv2"));
 	} finally {
 		await wsh.disconnect();
 	}
